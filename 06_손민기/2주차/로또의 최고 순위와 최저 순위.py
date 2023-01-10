bee6b1e19483226c312ea4693931c33164ca28f5
Hand_MK <thflgg133@naver.com>
2023-01-10 19:21:47 +0900

    Update 로또의 최고 순위와 최저 순위.py

--- 06_손민기/2주차/로또의 최고 순위와 최저 순위.py
+++ 06_손민기/2주차/로또의 최고 순위와 최저 순위.py
@@ -1,11 +1,11 @@
 def solution(lottos, win_nums):
     rank = [6, 6, 5, 4, 3, 2 ,1]
 
-    cnt_0 = lottos.count(0)
+    zeros = lottos.count(0)
     idx = 0
 
     for num in lottos:
         if num in win_nums:
             idx += 1
 
-    return [rank[idx + cnt_0], rank[idx]]
+    return [rank[idx + zeros], rank[idx]]
