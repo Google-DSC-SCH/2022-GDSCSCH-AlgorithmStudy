c7cfb18d8275823e34dd96783a921fe0af0497a2
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2023-01-12 14:50:09 +0900

    Update 01.로또의 최고 순위와 최저 순위.py

--- 34_최재원/01.로또의 최고 순위와 최저 순위.py
+++ 34_최재원/01.로또의 최고 순위와 최저 순위.py
@@ -6,8 +6,15 @@ def solution(lottos, win_nums):
             if n == a:
                 count += 1
                 break
-    rank = 7-count   #등수
-    answer = [rank-zero_count,rank]
+    a1=count+zero_count
+    a2=count
+    if a1 >= 2: 
+        a1=7-a1 
+    else: a1=6
+    if a2 >= 2: 
+        a2=7-a2 
+    else: a2=6
+    answer = [a1,a2]
     return answer
 
 lottos = [44, 1, 0, 0, 31, 25]
